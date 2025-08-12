9c2551525a3a7f50a6bd00d177f3db1ea000acd5
Manav khandelwal <114739289+manav-05-06@users.noreply.github.com>
2025-08-12 14:56:53 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,23 @@
+import { useEffect, useState } from 'react';
+
 function Footer() {
-    const now = new Date();
- 
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    // Set interval to update time every second
+    const interval = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+
+    // Clear interval when component unmounts
+    return () => clearInterval(interval);
+  }, []);
+
   return (
-    <footer className=" font-mono mt-20 py-6 text-center text-gray-600 dark:text-gray-400">
-      <p>&copy; {now.toLocaleTimeString()} Made by - Manav</p>
+    <footer className="font-mono mt-20 py-6 text-center text-gray-600 dark:text-gray-400">
+      <p>&copy; {time.toLocaleTimeString()} — Made by Manav</p>
     </footer>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
